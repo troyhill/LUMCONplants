91dfb7b118574b5d2bfa6b7b672d5bf857615cbb
Ryann Rossi <rrossi@lumcon.edu>
2019-02-13 14:47:51 -0600

    push 2.13.2019

--- R/ndvi_aboveground bm.R
+++ R/ndvi_aboveground bm.R
@@ -22,3 +22,29 @@ mass.ndvi <- ggplot(mydata, aes(x = mass, y = avg.ndvi)) + geom_point(aes(colour
 mass.ndvi
 #rsq is 0.33   
 
+##making text larger for figure
+exp_vs_coefs <- ggplot(aes(y = exp.live, x = coef.live, colour = seas), data = models) + geom_point(size=3) + theme_classic() + labs(y = "exponent", x = "coefficient") +
+  theme(text = element_text(size=14))
+# spring = low coefficients, high exponents
+##salt figure
+napp.salt.lum <- ggplot(napp.nuts[napp.nuts$region %in% "LUMCON", ], aes(x = salinity..psu., y = napp.smalley, colour = site)) + geom_point(size=3) + theme_classic() +  
+  labs(y = expression("Smalley NAPP (g "%.%m^-2%.%yr^-1~")"), x = "Creek salinity (psu)") + theme(text = element_text(size=14))  # weak!
+
+
+##no3 figure
+napp.no3.lum <- ggplot(napp.nuts[napp.nuts$region %in% "LUMCON", ], aes(x = dissolved.no3...no2..mm., y = napp.smalley, colour = site)) + geom_point(size=3) + theme_classic() + 
+  labs(y = expression("Smalley NAPP (g "%.%m^-2%.%yr^-1~")"), x = "Creekwater NO3 (uM)") + theme(text = element_text(size=14)) # weak
+
+
+#stem density figure
+live.stem<- summarySE(data=LUM, measurevar = "stems", groupvars=c("site", "yr", "mo"))
+dead.stem<- summarySE(data=LUM, measurevar = "stems.dead", groupvars=c("site", "yr", "mo"))
+
+ggplot(live.stem, 
+       aes(y = stems, x = as.numeric(time))) + #, col = region)) + 
+  geom_point(size = 1.2) + geom_errorbar(aes(ymin = (stmDens - stmDens.se), ymax = (stmDens + stmDens.se)), width = 0) +
+  geom_line(lwd = 0.7) +
+  theme_classic() + theme(legend.title = element_blank()) + 
+  ylab(expression("Live stem density (m"^-2~")")) + xlab("") + 
+  facet_wrap("group", nrow = 2) + 
+  theme_bw() %+replace% theme(strip.background  = element_blank(), legend.title = element_blank())
