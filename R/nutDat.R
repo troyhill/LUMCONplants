c28b51e5ad6b4b293b48dccdd5935c5ebbb5cb94
Troy Hill <Hill.Troy@gmail.com>
2019-02-13 15:33:22 -0500

    improved documentation, changed headers to be more descriptive.

--- R/nutDat.R
+++ R/nutDat.R
@@ -1,5 +1,44 @@
 #' plot-level data: soil and water nutrients and physical parameters
-#' @format A dataframe
+#' @format A dataframe the following variables:
+#' \describe{
+#' \item{year}{year of sample collection}
+#' \item{month.name}{month of sample collection (3-character form)}
+#' \item{month.number}{month of sample collection (numeric)}
+#' \item{region}{region}
+#' \item{site}{name of sampling marsh}
+#' \item{lat}{latitude, decimal degrees}
+#' \item{long}{longitude, decimal degrees}
+#' \item{dist}{distance from marsh edge, meters}
+#' \item{plot}{plot name (A, B, C)}
+#' \item{temp.soil}{soil temperature in degrees C}
+#' \item{ph.soil}{soil pH in pH units}
+#' \item{bulk.density.g.cm3.soil}{soil bulk density in grams per cubic centimeter}
+#' \item{grav.water.content.soil}{soil water content. Units = grams/gram dry mass}
+#' \item{om.soil}{organic matter content of soil collected in top 5 cm, measured by loss on ignition. Units = grams/gram}
+#' \item{c.soil}{organic carbon content of soil collected in top 5 cm and fumigated with HCl. Units = grams/gram}
+#' \item{tn.soil}{total nitrogen content of soil collected in top 5 cm and fumigated with HCl. Units = grams/gram}
+#' \item{tp.ppm.soil}{total phosphorous content of soil collected in top 5 cm and fumigated with HCl. Units = milligrams/gram}
+#' \item{extractable.no3.mmol.gdw.soil}{extractable NO3+NO2 in soil. Units = millimol N per gram dry mass}
+#' \item{extractable.nh4.mmol.gdw.soil}{extractable NH4 in soil. Units = millimol N per gram dry mass}
+#' \item{extractable.po4.mmol.gdw.soil}{extractable PO4 in soil. Units = millimol P per gram dry mass}
+#' \item{porewater.salinity.soil}{porewater salinity. Units = psu}
+#' \item{redox.soil}{soil redox. Units = milliVolts}
+#' \item{water.temp.bay}{temperature of bay water adjacent to site. Units = degrees C}
+#' \item{salinity.psu.bay}{salinity of bay water adjacent to site. Units = psu}
+#' \item{sp.cond.bay}{specific conductivity of  bay water adjacent to site. Units = mS/cm}
+#' \item{dissolved.no3.no2.bay}{dissolved NO3+NO2 in bay water adjacent to site. Units = millimoles N/L}
+#' \item{dissolved.nh4.bay}{dissolved NH4 in bay water adjacent to site. Units = millimoles N/L}
+#' \item{dissolved.po4.bay}{dissolved PO4 in bay water adjacent to site. Units = millimoles P/L}
+#' \item{dissolved.sio2.bay}{dissolved SiO2 in bay water adjacent to site. Units = millimoles Si/L}
+#' \item{water.depth.cm.}{depth of overlying water at marsh sampling location. Units = centimeters}
+#' \item{water.temp}{temperature of overlying water at marsh sampling location. Units = degrees Celsius}
+#' \item{salinity.psu.}{salinity of overlying water at marsh sampling location. Units = psu}
+#' \item{sp.cond}{specific conductivity of overlying water at marsh sampling location. Units = mS/cm}
+#' \item{dissolved.no3.no2}{dissolved NO3+NO2 in overlying water at marsh sampling location. Units = millimoles N/L}
+#' \item{dissolved.nh4}{dissolved NH4 in overlying water at marsh sampling location. Units = millimoles N/L}
+#' \item{dissolved.po4}{dissolved PO4 in overlying water at marsh sampling location. Units = millimoles P/L}
+#' \item{dissolved.sio2}{dissolved SiO2 in overlying water at marsh sampling location. Units = millimoles Si/L}
+#' }
 #' @docType data
 #' @keywords datasets
 #' @name nutDat
@@ -26,7 +65,7 @@ NULL
 # ### This data is in C:\Users\tdh\Documents\LUMCON\Allometry\GRIIDC\dataset12_all_years_20190110.xlsx
 # ### It will continue to be updated
 # 
-# nutDat <- read.delim("C:/RDATA/LUMCON/allometry_preprocessing/plot_data/data_soil_water_all_years_20190109.txt", skip = 2, stringsAsFactors = FALSE)
+# nutDat <- read.delim("C:/RDATA/LUMCON/allometry_preprocessing/plot_data/data_soil_water_all_years_20190213.txt", skip = 2, stringsAsFactors = FALSE)
 # names(nutDat) <- tolower(names(nutDat))
 # nutDat$site   <- gsub(x = trimws(nutDat$site), pattern = " ", replacement = "")
 # nutDat$plot   <- gsub(x = trimws(nutDat$plot), pattern = " |[1-9]", replacement = "")
--- R/stemDat.R
+++ R/stemDat.R
@@ -1,5 +1,33 @@
 #' Stem-level dataset with plant heights and masses
-#' @format A dataframe
+#' @format A dataframe with the following columns:
+#' \describe{
+#' \item{yr}{year of sample collection}
+#' \item{mo}{month of sample collection (3-character form)}
+#' \item{mo2}{month of sample collection (numeric)}
+#' \item{region}{region}
+#' \item{site}{name of sampling marsh}
+#' \item{lat}{latitude, decimal degrees}
+#' \item{long}{longitude, decimal degrees}
+#' \item{dist}{distance from marsh edge, meters}
+#' \item{id}{unique identifier for individually-weighed stems}
+#' \item{status}{live/dead status of plant}
+#' \item{hgt}{height from plant base to longest outstretched point. Units = centimeters}
+#' \item{mass_tot}{total mass of plant. Units = grams}
+#' \item{mass_s}{stem mass. Units = grams}
+#' \item{mass_l}{leaf mass. Units = grams}
+#' \item{flwr}{dummy variable indicating flowering status (1 = inflorescence/infructescence present). Inconsistently censused.}
+#' \item{flwr_len}{length of inflorescence/infructescence. Units = centimeters}
+#' \item{flwr_mass}{mass of inflorescence/infructescence. Units = grams}
+#' \item{moYr}{month-year of sampling (form = "%b-%Y")}
+#' \item{time}{month-year of sampling as yearmon data type}
+#' \item{season}{season-year of sampling}
+#' \item{seas}{season of sampling}
+#' \item{plantYr}{year of sampling (modifed so as not to split the winter between calendar years)}
+#' \item{mass.est1}{plant mass estimated from allometry. Units = grams}
+#' \item{mass.est2}{plant mass estimated from allometry. Units = grams}
+#' \item{modeled}{dummy variable indicating whether final plant mass is directly measured (= 0) or estimated from allometry model (= 1)}
+#' \item{mass}{best estimate of plant mass; directly measured, when available. Otherwise, estimated from allometry models. Units = grams}
+#' }
 #' @docType data
 #' @keywords datasets
 #' @name stemDat
